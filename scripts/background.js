c7bb455663e73259c8e18dda9036d097c3b2b0d0
Stigmat <castlewania.alex@gmail.com>
2018-12-17 22:02:08 +0300

    get domain from url & add unique domains in list

--- scripts/background.js
+++ scripts/background.js
@@ -15,12 +15,45 @@ WebTimer.prototype = {
 
     addTab: function (tab) {
         if (this.isValidPage(tab) === true) {
-            if (tab.id && (tab.id != 0)) {
+            if (tab.id && (tab.id != 0)) { 
                 this.tabs = this.tabs || [];
-                this.tabs.push(tab.url);
-                this.activeTab = tab.url;
+                var domain = this.extractRootDomain(tab.url);
+                if (!this.tabs.includes(domain)){
+                    this.tabs.push(domain);
+                }
+                this.activeTab = domain;
             }
         }
+    },
+
+    extractRootDomain: function (url) {
+        var domain = this.extractHostname(url),
+            splitArr = domain.split('.'),
+            arrLen = splitArr.length;
+    
+        if (arrLen > 2) {
+            domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+            if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
+                domain = splitArr[arrLen - 3] + '.' + domain;
+            }
+        }
+        return domain;
+    },
+
+    extractHostname: function(url) {
+        var hostname;
+    
+        if (url.indexOf("//") > -1) {
+            hostname = url.split('/')[2];
+        }
+        else {
+            hostname = url.split('/')[0];
+        }
+    
+        hostname = hostname.split(':')[0];
+        hostname = hostname.split('?')[0];
+
+        return hostname;
     },
 
     isValidPage: function (tab) {
