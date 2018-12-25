558099cb90889661e17b0fb9465bae98a25e5084
Hrisanfov Alexey <hai@altx-soft.ru>
2018-12-25 15:31:14 +0300

    fix start timer

--- scripts/background.js
+++ scripts/background.js
@@ -14,22 +14,24 @@ function updateStorage() {
 
 function backgroundCheck() {
     chrome.windows.getLastFocused({ populate: true }, function (currentWindow) {
-        if (tabs.length > 0) {
-            var activeTab = currentWindow.tabs.find(o => o.active === true);
-            if (activeTab !== undefined) {
+        if (currentWindow.focused) {
+            var activeTab = currentWindow.tabs.find(t => t.active === true);
+            if (activeTab !== undefined && activity.isValidPage(activeTab)) {
                 var activeUrl = activity.extractHostname(activeTab.url);
                 var tab = activity.getTab(activeUrl);
-                if (tab !== undefined) {
-                    chrome.idle.queryState(SETTINGS_INTERVAL_INACTIVITY, function (state) {
-                        if (state === 'active') {
-                            tab.summaryTime += 1;
-                            chrome.browserAction.setBadgeText({
-                                tabId: activeTab.id,
-                                text: String(convertSummaryTimeToString(tab.summaryTime))
-                            });
-                        }
-                    });
+                if (tab === undefined) {
+                    activity.addTab(activeTab);
                 }
+
+                chrome.idle.queryState(SETTINGS_INTERVAL_INACTIVITY, function (state) {
+                    if (state === 'active') {
+                        tab.summaryTime += 1;
+                        chrome.browserAction.setBadgeText({
+                            tabId: activeTab.id,
+                            text: String(convertSummaryTimeToString(tab.summaryTime))
+                        });
+                    }
+                });
             }
         }
     });
