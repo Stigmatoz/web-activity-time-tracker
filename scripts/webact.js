b06236e3940d6f06cebf99a6efc86d039cb82c86
Stigmat <castlewania.alex@gmail.com>
2019-01-15 01:45:01 +0300

    add settings page

--- scripts/webact.js
+++ scripts/webact.js
@@ -24,6 +24,13 @@ document.addEventListener('DOMContentLoaded', function () {
         ui.setUIForByDays();
         getDataFromStorageByDays();
     });
+    document.getElementById('settings').addEventListener('click', function () {
+        if (chrome.runtime.openOptionsPage) {
+            chrome.runtime.openOptionsPage();
+          } else {
+            window.open(chrome.runtime.getURL('options.html'));
+          }
+    });
 });
 
 firstInitPage();
