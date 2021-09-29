600906d791b6d9b7bda0189301902d7c02dd8e80
Alex <castlewania.alex@gmail.com>
2021-09-29 17:34:49 +0300

    Fixed site name to view restricted page

--- src/scripts/block.js
+++ src/scripts/block.js
@@ -1,43 +1,50 @@
-'use strict';
+"use strict";
 
 var storage = new LocalStorage();
 var blockSiteUrl;
 var restrictionList = [];
 
-document.addEventListener('DOMContentLoaded', function () {
-    var url = new URL(document.URL);
-    blockSiteUrl = url.searchParams.get('url');
-    document.getElementById('site').innerText = extractHostname(blockSiteUrl);
+document.addEventListener("DOMContentLoaded", function () {
+  var url = new URL(document.URL);
+  blockSiteUrl = url.searchParams.get("url");
+  document.getElementById("site").innerText = extractHostname(blockSiteUrl);
 
-    storage.getValue(STORAGE_RESTRICTION_LIST, function (items) {
-        restrictionList = items;
-        if (restrictionList === undefined)
-            restrictionList = [];
-        var currentItem = restrictionList.find(x => isDomainEquals(x.domain, blockSiteUrl));
-        if (currentItem !== undefined){
-            document.getElementById('limit').innerText = convertShortSummaryTimeToString(currentItem.time);
-        }
-    });
+  storage.getValue(STORAGE_RESTRICTION_LIST, function (items) {
+    restrictionList = items;
+    if (restrictionList === undefined) restrictionList = [];
+    var currentItem = restrictionList.find((x) =>
+      isDomainEquals(extractHostname(x.domain), extractHostname(blockSiteUrl))
+    );
+    if (currentItem !== undefined) {
+      document.getElementById("limit").innerText =
+        convertShortSummaryTimeToString(currentItem.time);
+    }
+  });
 
-    storage.getValue(SETTINGS_BLOCK_DEFERRAL, function(item) { 
-        var deferBtn = document.getElementById('deffererBtn');
-        if (item){
-            deferBtn.addEventListener('click', function () {
-                chrome.runtime.getBackgroundPage(function (bg) {
-                    let defList = bg.deferredRestrictionsList;
-                    if (defList == undefined)
-                        defList = [];
-                    defList.push({ site: blockSiteUrl, dateOfDeferred: new Date().getTime()});
+  storage.getValue(SETTINGS_BLOCK_DEFERRAL, function (item) {
+    var deferBtn = document.getElementById("deffererBtn");
+    if (item) {
+      deferBtn.addEventListener("click", function () {
+        chrome.runtime.getBackgroundPage(function (bg) {
+          let defList = bg.deferredRestrictionsList;
+          if (defList == undefined) defList = [];
+          defList.push({
+            site: blockSiteUrl,
+            dateOfDeferred: new Date().getTime(),
+          });
 
-                    bg.deferredRestrictionsList = defList;
+          bg.deferredRestrictionsList = defList;
 
-                    chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
-                        chrome.tabs.update(tab.id, { url: blockSiteUrl });
-                    });
-                });
-            });
-        } else {
-            deferBtn.remove();
-        }
-    });
+          chrome.tabs.query(
+            { currentWindow: true, active: true },
+            function (tab) {
+              chrome.tabs.update(tab.id, { url: blockSiteUrl });
+            }
+          );
+        });
+      });
+    } else {
+      deferBtn.remove();
+    }
+  });
 });
