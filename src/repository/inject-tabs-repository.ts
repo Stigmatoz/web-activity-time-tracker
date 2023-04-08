9573fa8673b2acf472198f391fbcf9fb23ac177c
Alex <castlewania.alex@gmail.com>
2023-04-08 16:50:17 +0300

    Tab repository as singleton

--- src/repository/inject-tabs-repository.ts
+++ src/repository/inject-tabs-repository.ts
@@ -1,8 +1,18 @@
 import { ITabsRepository } from "./tabs-repository-interface";
 import { TabsRepository } from "./tabs-repository";
 
+let instanse: ITabsRepository | null = null;
+
+async function createAndInitInstance() {
+  let repo = new TabsRepository();
+  await repo.initAsync();
+  return repo;
+}
+
 export async function injectTabsRepository(): Promise<ITabsRepository> {
-    const repo = new TabsRepository();
-    await repo.initAsync();
-    return repo;
+  if (instanse == null) {
+    console.log("test");
+    instanse = await createAndInitInstance();
   }
+  return instanse;
+}
