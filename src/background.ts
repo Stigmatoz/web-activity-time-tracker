cf07e61d091d739743d482fd8bf2fcee170118b4
Alex <castlewania.alex@gmail.com>
2023-05-18 15:25:29 +0300

    Change logic for updating favicon and remove permission "webNavigation"

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,15 @@
-import Browser from "webextension-polyfill";
-import { initTracker } from "./tracker";
-import { logger } from "./compositions/logger";
-import { useFavicon } from "./compositions/update-favicon";
+import Browser from 'webextension-polyfill';
+import { initTracker } from './tracker';
+import { logger } from './compositions/logger';
+import { useFavicon } from './compositions/update-favicon';
 
-logger.log("Start background script");
+logger.log('Start background script');
 
-Browser.runtime.onInstalled.addListener((details) => {
-  logger.log("Extension installed:", details);
+Browser.runtime.onInstalled.addListener(details => {
+  logger.log('Extension installed:', details);
 });
 
-Browser.webNavigation.onCompleted.addListener(async function(details) {
+Browser.webNavigation.onCompleted.addListener(async function (details) {
   await useFavicon(details.tabId);
 });
 
--- src/tracker.ts
+++ src/tracker.ts
@@ -1,112 +1,124 @@
-import Browser from "webextension-polyfill";
-import { isValidPage } from "./compositions/valid-page";
-import { extractHostname } from "./compositions/extract-hostname";
-import { injectTabsRepository } from "./repository/inject-tabs-repository";
-import { isInBlackList } from "./compositions/black-list";
-import { useBadge } from "./compositions/set-badge";
-import { INTERVAL_INACTIVITY_DEFAULT, INTERVAL_SAVE_STORAGE_DEFAULT, StorageParams, VIEW_TIME_IN_BADGE_DEFAULT,  } from "./storage/storage-params";
-import { BadgeColor } from "./compositions/set-badge";
-import { injecStorage } from "./storage/inject-storage";
-import { addInterval, closeInterval } from "./compositions/daily-intervals";
-import { ActiveTab } from "./compositions/activeTab";
-import { isLimitExceeded } from "./compositions/limit-list";
-import { Tab } from "./entity/tab";
-import { useBlockPage } from "./compositions/block-page";
-import { convertSummaryTimeToBadgeString } from "./utils/converter";
+import Browser from 'webextension-polyfill';
+import { isValidPage } from './compositions/valid-page';
+import { extractHostname } from './compositions/extract-hostname';
+import { injectTabsRepository } from './repository/inject-tabs-repository';
+import { isInBlackList } from './compositions/black-list';
+import { useBadge } from './compositions/set-badge';
+import {
+  INTERVAL_INACTIVITY_DEFAULT,
+  INTERVAL_SAVE_STORAGE_DEFAULT,
+  StorageParams,
+  VIEW_TIME_IN_BADGE_DEFAULT,
+} from './storage/storage-params';
+import { BadgeColor } from './compositions/set-badge';
+import { injecStorage } from './storage/inject-storage';
+import { addInterval, closeInterval } from './compositions/daily-intervals';
+import { ActiveTab } from './compositions/activeTab';
+import { isLimitExceeded } from './compositions/limit-list';
+import { Tab } from './entity/tab';
+import { useBlockPage } from './compositions/block-page';
+import { convertSummaryTimeToBadgeString } from './utils/converter';
 
 const activeTabInstance = ActiveTab.getInstance();
 const storage = injecStorage();
 
 export async function initTracker() {
-    setInterval(trackTime, 1000);
-    setInterval(saveTabs, INTERVAL_SAVE_STORAGE_DEFAULT);
+  setInterval(trackTime, 1000);
+  setInterval(saveTabs, INTERVAL_SAVE_STORAGE_DEFAULT);
 }
 
-async function trackTime(){
-    const repo = await injectTabsRepository();
-    const window = await Browser.windows.getLastFocused({ populate: true });
-    if (window.focused) {
-        const activeTab = window.tabs?.find((t) => t.active === true);
-        if (isValidPage(activeTab)) {
-            const activeDomain = extractHostname(activeTab!.url);
+async function trackTime() {
+  const repo = await injectTabsRepository();
+  const window = await Browser.windows.getLastFocused({ populate: true });
+  if (window.focused) {
+    const activeTab = window.tabs?.find(t => t.active === true);
+    if (isValidPage(activeTab)) {
+      const activeDomain = extractHostname(activeTab!.url);
 
-            if (await isInBlackList(activeDomain)) {
-                useBadge({
-                    tabId: activeTab!.id!,
-                    text: "n/a",
-                    color: BadgeColor.red,
-                });
-            } else {
-                let tab = repo.getTab(activeDomain);
-                if (tab == undefined) {
-                    tab = await repo.addTab(activeDomain, activeTab?.favIconUrl);
-                }
-                if (tab != undefined){
-                    const inactivityInterval = (await storage.getValue(
-                        StorageParams.INTERVAL_INACTIVITY,
-                        INTERVAL_INACTIVITY_DEFAULT
-                    )) as number;
-                    const state = await Browser.idle.queryState(inactivityInterval);
-                    mainTracker(state, activeTab!, activeDomain, tab);
-                }
-            }
+      if (await isInBlackList(activeDomain)) {
+        useBadge({
+          tabId: activeTab!.id!,
+          text: 'n/a',
+          color: BadgeColor.red,
+        });
+      } else {
+        let tab = repo.getTab(activeDomain);
+        if (tab == undefined) {
+          tab = await repo.addTab(activeDomain, activeTab?.favIconUrl);
         }
-    } else {
-        await closeInterval(activeTabInstance.getActiveTabDomain());
-        activeTabInstance.setActiveTab(null);
+        if (tab != undefined) {
+          const inactivityInterval = (await storage.getValue(
+            StorageParams.INTERVAL_INACTIVITY,
+            INTERVAL_INACTIVITY_DEFAULT,
+          )) as number;
+          const state = await Browser.idle.queryState(inactivityInterval);
+          mainTracker(state, activeTab!, activeDomain, tab);
+        }
+      }
     }
+  } else {
+    await closeInterval(activeTabInstance.getActiveTabDomain());
+    activeTabInstance.setActiveTab(null);
+  }
 }
 
-async function mainTracker(state: Browser.Idle.IdleState, activeTab: Browser.Tabs.Tab, activeDomain: string, tab: Tab){
-    function isAudible(){
-        return state === 'idle' && activeTab.audible;
+async function mainTracker(
+  state: Browser.Idle.IdleState,
+  activeTab: Browser.Tabs.Tab,
+  activeDomain: string,
+  tab: Tab,
+) {
+  function isAudible() {
+    return state === 'idle' && activeTab.audible;
+  }
+
+  function isActiveTabWasChanged() {
+    return activeDomain != activeTabInstance.getActiveTabDomain();
+  }
+
+  const isAudibleValue = isAudible();
+  if (state === 'active' || isAudibleValue) {
+    if (await isLimitExceeded(activeDomain, tab)) {
+      const summaryTime = tab.days.at(-1)!.summary;
+      const summaryCounter = tab.days.at(-1)!.counter;
+      await useBlockPage(activeDomain, summaryTime, summaryCounter);
+      return;
     }
 
-    function isActiveTabWasChanged(){
-        return activeDomain != activeTabInstance.getActiveTabDomain();
+    if (isActiveTabWasChanged()) {
+      tab.incCounter();
+      await closeInterval(activeTabInstance.getActiveTabDomain());
+      activeTabInstance.setActiveTab(activeTab.url!);
+      await addInterval(activeTabInstance.getActiveTabDomain());
     }
+    if (tab.favicon == '' && activeTab.favIconUrl != undefined)
+      tab.setFavicon(activeTab.favIconUrl);
 
-    const isAudibleValue = isAudible();
-    if (state === 'active' || isAudibleValue){
-        if (await isLimitExceeded(activeDomain, tab)){
-            const summaryTime = tab.days.at(-1)!.summary;
-            const summaryCounter = tab.days.at(-1)!.counter;
-            await useBlockPage(activeDomain, summaryTime, summaryCounter);
-            return;
-        }
+    tab.incSummaryTime();
 
-        if (isActiveTabWasChanged()){
-            tab.incCounter();
-            await closeInterval(activeTabInstance.getActiveTabDomain());
-            activeTabInstance.setActiveTab(activeTab.url!);
-            await addInterval(activeTabInstance.getActiveTabDomain());
-        }
-        tab.incSummaryTime();
+    const viewInBadge = (await storage.getValue(
+      StorageParams.VIEW_TIME_IN_BADGE,
+      VIEW_TIME_IN_BADGE_DEFAULT,
+    )) as boolean;
 
-        const storage = injecStorage();
-        const viewInBadge = (await storage.getValue(
-            StorageParams.VIEW_TIME_IN_BADGE,
-            VIEW_TIME_IN_BADGE_DEFAULT
-        )) as boolean;
-
-        if (viewInBadge)
-            useBadge({
-                tabId: activeTab!.id!,
-                text: convertSummaryTimeToBadgeString(tab.days.at(-1)!.summary),
-                color: BadgeColor.green,
-            });
-        else 
-            useBadge({
-                tabId: activeTab!.id!,
-                text: "",
-                color: BadgeColor.red,
-            });
-    }
+    if (viewInBadge)
+      useBadge({
+        tabId: activeTab!.id!,
+        text: convertSummaryTimeToBadgeString(tab.days.at(-1)!.summary),
+        color: BadgeColor.green,
+      });
+    else
+      useBadge({
+        tabId: activeTab!.id!,
+        text: '',
+        color: BadgeColor.red,
+      });
+  }
 }
 
-async function saveTabs(){
-    const storage = injecStorage();
-    const repo = await injectTabsRepository();
-    const tabs = repo.getTabs();
-    await storage.saveTabs(tabs);
+async function saveTabs() {
+  const storage = injecStorage();
+  const repo = await injectTabsRepository();
+  const tabs = repo.getTabs();
+  await storage.saveTabs(tabs);
 }
