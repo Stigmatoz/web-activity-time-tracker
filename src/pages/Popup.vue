e9ad89af949867ccf5692a21554e81f64c370ec0
Alex <castlewania.alex@gmail.com>
2023-05-26 01:42:14 +0300

    Tabs on popup

--- src/pages/Popup.vue
+++ src/pages/Popup.vue
@@ -6,12 +6,50 @@
       <img height="17" src="../assets/icons/settings.svg" @click="openSettings()" />
     </div>
   </div>
-  <TabList />
+  <div class="tabs">
+    <input type="radio" id="todayTab" name="tab-control" checked />
+    <input type="radio" id="allTimeTab" name="tab-control" />
+    <input type="radio" id="byDaysTab" name="tab-control" />
+    <ul>
+      <li title="Today">
+        <label for="todayTab" role="button"
+          ><img height="15" src="../assets/icons/24-hours.svg" /><span>Today</span></label
+        >
+      </li>
+      <li title="All The Time">
+        <label for="allTimeTab" role="button"
+          ><img height="15" src="../assets/icons/pie-chart.svg" /><span>All The Time</span></label
+        >
+      </li>
+      <li title="By Days">
+        <label for="byDaysTab" role="button"
+          ><img height="15" src="../assets/icons/stats.svg" /><span>By Days</span></label
+        >
+      </li>
+    </ul>
+
+    <div class="slider"><div class="indicator"></div></div>
+    <div class="content">
+      <section>
+        <TabList :type="TypeOfList.Today" />
+      </section>
+      <section>
+        <h2>Delivery Contents</h2>
+      </section>
+      <section>
+        <h2>Shipping</h2>
+      </section>
+      <section>
+        <h2>Returns</h2>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import Browser from 'webextension-polyfill';
 import TabList from '../components/TabList.vue';
+import { TypeOfList } from '../utils/enums';
 
 async function openSettings() {
   await Browser.tabs.create({
@@ -23,13 +61,14 @@ async function openSettings() {
 
 <style scoped>
 .headerBlock {
-  height: 50px;
-  background-color: var(--main-color);
+  height: 40px;
+  background-color: var(--popup-header);
 }
 
 .headerBlock .header {
   font-size: 16px;
   padding: 0 20px;
+  margin-top: 10px;
   display: inline-block;
   font-weight: 600;
   color: rgb(0, 0, 0);
@@ -37,6 +76,6 @@ async function openSettings() {
 
 .headerBlock img {
   cursor: pointer;
-  padding: 15px 10px;
+  padding: 10px;
 }
 </style>
