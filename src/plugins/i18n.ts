c9676e8379fa0643f531a0ef9c08de89cf6f33cb
Alex <castlewania.alex@gmail.com>
2023-07-15 18:31:05 +0300

    Set default language according to browser language

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,11 +1,12 @@
 import { createI18n } from 'vue-i18n';
+import Browser from 'webextension-polyfill';
 import en from '../_locales/en/messages.json';
 import ru from '../_locales/ru/messages.json';
 
 const i18n = createI18n({
   legacy: false,
-  locale: import.meta.env.VITE_I18N_LOCALE || 'en',
-  fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE || 'en',
+  locale: Browser.i18n.getUILanguage(),
+  fallbackLocale: 'en',
   globalInjection: true,
   messages: {
     en,
