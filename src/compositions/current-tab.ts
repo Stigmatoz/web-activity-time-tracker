3a02fefd72b654e55735c0dfac622af58c1f78d2
Alex <castlewania.alex@gmail.com>
2024-03-05 15:43:23 +0300

    Remove old functions after rename folder

--- src/compositions/current-tab.ts
+++
@@ -1,8 +0,0 @@
-import Browser from 'webextension-polyfill';
-
-export async function getCurrentTab() {
-  const tabs = await Browser.tabs.query({ active: true, currentWindow: true });
-  // since only one tab should be active and in the current window at once
-  // the return variable should only have one entry
-  return tabs[0];
-}
--- src/compositions/useRemoveAllData.ts
+++
@@ -1,9 +0,0 @@
-import Browser from 'webextension-polyfill';
-import { injectStorage } from '../storage/inject-storage';
-import { Messages } from '../utils/messages';
-
-export async function useRemoveAllData() {
-  const storage = injectStorage();
-  await storage.saveIntervalList([]);
-  await Browser.runtime.sendMessage(Messages.ClearAllData);
-}
