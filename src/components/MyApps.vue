a988f5d7b2ed87472f914cfe80a4a596aea8ea6c
Alex <castlewania.alex@gmail.com>
2024-08-06 13:44:03 +0300

    Remove trackerjam

--- src/components/MyApps.vue
+++ src/components/MyApps.vue
@@ -12,17 +12,6 @@
         </p>
       </div>
     </div>
-    <div class="app-block" @click="openAppLink(App.TrackerJam)">
-      <div class="img-block">
-        <img src="../assets/icons/trackerjam-logo.png" height="45" />
-      </div>
-      <div>
-        <p class="title">{{ t('trackerjam.message') }}</p>
-        <p class="description">
-          {{ t('trackerjam.description') }}
-        </p>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -39,16 +28,12 @@ const { t } = useI18n();
 
 enum App {
   ClearYoutube,
-  TrackerJam,
 }
 
 function openAppLink(app: App) {
   switch (app) {
     case App.ClearYoutube:
       window.open(CHROME_STORE_CLEAR_YOUTUBE_URL, '_blank');
-      break;
-    case App.TrackerJam:
-      window.open('https://trackerjam.com/?utm_source=watt_extension', '_blank');
       break;
   }
 }
