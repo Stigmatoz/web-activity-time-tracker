37944ac6b841e1a3958346b3b37ca0ffe1860cb7
Alex <castlewania.alex@gmail.com>
2024-08-14 11:19:42 +0300

    Update resources and add utm source

--- src/utils/chrome-url.ts
+++ src/utils/chrome-url.ts
@@ -1,4 +1,4 @@
 export const CHROME_STORE_REVIEW_URL = `https://chromewebstore.google.com/detail/web-activity-time-tracker/${__APP_ID__}/reviews`;
 export const CHROME_STORE_SUPPORT_URL = `https://chromewebstore.google.com/detail/web-activity-time-tracker/${__APP_ID__}/support`;
 export const CHROME_STORE_CLEAR_YOUTUBE_URL =
-  'https://chromewebstore.google.com/detail/clean-youtube-update-yout/kalhfjomailhflienkfajocjodgjipie';
+  'https://chromewebstore.google.com/detail/clean-youtube-update-yout/kalhfjomailhflienkfajocjodgjipie?utm_source=watt';
