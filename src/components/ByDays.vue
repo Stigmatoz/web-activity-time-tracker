5be1b4087a6e588bb793f00ff508b8f273a999ba
Даниил <73600007+daniilium@users.noreply.github.com>
2024-09-21 15:34:41 +0300

    feat: add total time for daily tab (#141)

--- src/components/ByDays.vue
+++ src/components/ByDays.vue
@@ -2,8 +2,10 @@
   <div class="no-data" v-if="isLoading">
     <img height="55" src="../assets/icons/preloader.gif" />
   </div>
+
   <div v-else>
     <no-data-by-days v-if="countOfDays == undefined || (countOfDays == 0 && !noData)" />
+
     <div v-else-if="noData">
       <div class="no-data">
         {{ t('noDataForPeriod.message') }}
@@ -17,10 +19,11 @@
         >
           <template #yearly="{ label, range, presetDateRange }">
             <span @click="presetDateRange(range)">{{ label }}</span>
-          </template></VueDatePicker
-        >
+          </template>
+        </VueDatePicker>
       </div>
     </div>
+
     <div v-else>
       <div class="date-block">
         <VueDatePicker
@@ -33,29 +36,40 @@
         >
           <template #yearly="{ label, range, presetDateRange }">
             <span @click="presetDateRange(range)">{{ label }}</span>
-          </template></VueDatePicker
-        >
+          </template>
+        </VueDatePicker>
         <input type="button" :value="t('exportToCsv.message')" @click="exportToCsv()" />
       </div>
+
       <div class="stats-block block">
         <div class="header">{{ t('averageTimeByDays.message') }}</div>
         <p>{{ convertSummaryTimeToString(tabsByDays!.averageTime) }}</p>
       </div>
+
       <div class="ml-20 mr-20 by-days-chart">
         <ByDaysChart :data="tabsByDays!" />
       </div>
+
       <div>
+        <Expander day="Total" :time="tabsByDays?.summaryTime || 0">
+          <TabItem
+            v-for="(tab, i) of mergeAllDays(tabsByDays?.days)"
+            :item="tab"
+            :summaryTimeForWholeDay="tabsByDays?.summaryTime || 0"
+          />
+        </Expander>
+
         <Expander
           v-for="(tabDay, i) of tabsByDays?.days"
           :key="i"
           :day="tabDay.day"
           :time="tabDay.time"
         >
-          <TabItem
-            v-for="(tab, i) of tabDay.tabs"
-            :key="i"
-            :item="tab"
-            :summaryTimeForWholeDay="tabDay.time"
+        <TabItem
+          v-for="(tab, i) of tabDay.tabs"
+          :key="i"
+          :item="tab"
+          :summaryTimeForWholeDay="tabDay.time"
           />
         </Expander>
       </div>
@@ -77,6 +91,7 @@ import NoDataByDays from './NoDataByDays.vue';
 import ByDaysChart from '../components/ByDaysChart.vue';
 import Expander from '../components/Expander.vue';
 import { TabListByDays } from '../dto/tabListSummary';
+import { CurrentTabItem } from '../dto/currentTabItem';
 import { useTabListByDays } from '../functions/useTabListByDays';
 import { convertSummaryTimeToString } from '../utils/converter';
 import { ranges, ThisWeekRange } from '../utils/date';
@@ -130,6 +145,27 @@ async function exportToCsv() {
     FileType.CSV,
     `websites_${dateFrom.toLocaleDateString()}-${dateTo.toLocaleDateString()}.csv`,
   );
+}
+
+function mergeAllDays(days: TabListByDays['days'] | undefined): CurrentTabItem[] {
+  if (!days) return [];
+
+  const urlMap = new Map<string, CurrentTabItem>();
+
+  for (const day of days) {
+    for (const tab of day.tabs) {
+      if (!urlMap.has(tab.url)) {
+        urlMap.set(tab.url, { ...tab });
+        continue;
+      }
+
+      const existingTab = urlMap.get(tab.url)!;
+      existingTab.summaryTime += tab.summaryTime;
+      existingTab.sessions += tab.sessions;
+    }
+  }
+
+  return Array.from(urlMap.values()).sort((a, b) => b.summaryTime - a.summaryTime);
 }
 </script>
 
