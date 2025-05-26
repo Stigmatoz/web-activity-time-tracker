0e72a55c72e19b84202e1d37f1d0559a4d2fe7c4
Alex <castlewania.alex@gmail.com>
2025-05-26 18:05:20 +0300

    Change package name

--- scripts/zip.ps1
+++ scripts/zip.ps1
@@ -4,4 +4,4 @@ If (!(test-path -PathType container $targetFolder)) {
     New-Item -ItemType Directory -Path $targetFolder
 }
 $version = $(node -pe "require('./src/manifest.json').version")
-Compress-Archive -Path $sourceFolder -DestinationPath $targetFolder/web-activity-time-tracker-$version.zip
+Compress-Archive -Path $sourceFolder -DestinationPath $targetFolder/time-spy-$version.zip
